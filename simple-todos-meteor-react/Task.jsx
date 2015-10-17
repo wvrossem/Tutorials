45905b753696437f83cba23a763d958e1216d6bb
Wouter Van Rossem <woutervanrossem@gmail.com>
2015-10-17 15:07:04 +0200

    Initial commit simple todos app Meteor+React

---
+++ simple-todos-meteor-react/Task.jsx
@@ -0,0 +1,12 @@
+
+Task = React.createClass({
+    propTypes: {
+    task: React.PropTypes.object.isRequired
+    },
+
+    render() {
+        return (
+            <li>{this.props.task.text}</li>
+        );
+    }
+});
