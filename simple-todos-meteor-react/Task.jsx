c5bc769f77a91ba8bb8801bd0b3479d075576dea
Wouter Van Rossem <woutervanrossem@gmail.com>
2015-10-17 21:32:02 +0200

    Added checking & deletion of tasks

--- simple-todos-meteor-react/Task.jsx
+++ simple-todos-meteor-react/Task.jsx
@@ -4,9 +4,32 @@ Task = React.createClass({
     	task: React.PropTypes.object.isRequired
     },
 
+    toggleChecked() {
+    	Tasks.update(this.props.task._id, {
+    		$set: {checked: !this.props.task.checked}
+    	});
+    },
+
+    deleteThisTask() {
+    	Tasks.remove(this.props.task._id);
+    },
+
     render() {
+    	const taskClassname = this.props.task.checked ? "checked" : "";
+
         return (
-            <li>{this.props.task.text}</li>
+            <li className={taskClassname}>
+            	<button className="delete" onClick={this.deleteThisTask}>
+            		&times;
+            	</button>
+            	<input
+            		type="checkbox"
+        			readOnly={true}
+        			checked={this.props.task.checked}
+       				onClick={this.toggleChecked} />
+ 
+        		<span className="text">{this.props.task.text}</span>
+        	</li>
         );
     }
 });
