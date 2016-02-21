0b999fec3fd284d6aa5ec3cdfc3214c1eaf141ef
Wouter Van Rossem <woutervanrossem@gmail.com>
2016-02-21 10:35:21 +0100

    NodeJS - Using spawn process

--- NodeJS-the-right-way/Chapter2/watcher.js
+++ NodeJS-the-right-way/Chapter2/watcher.js
@@ -1,13 +1,16 @@
+"use strict";
+
 const 
-	fs = require('fs'),
+	fs       = require('fs'),
+	spawn    = require('child_process').spawn,
 	filename = process.argv[2];
 
 if(!filename) {
-	throw Error("A filename must be specified")
+	throw Error("A filename must be specified");
 }
 
 fs.watch(filename, function() {
-	console.log("File " + filename + " just changed");
-	console.log("Watching " + filename + " for changes...");
+	let ls = spawn('ls', ['-lh', filename]);
+	ls.stdout.pipe(process.stdout);
+	console.log("Waching " + filename + " for changes");
 });
-
