38005dbecc04278498c544e31921a30da796d20d
Wouter Van Rossem <woutervanrossem@gmail.com>
2016-02-21 10:17:17 +0100

    NodeJS-the-right-way first commit

---
+++ NodeJS-the-right-way/Chapter2/watcher.js
@@ -0,0 +1,13 @@
+const 
+	fs = require('fs'),
+	filename = process.argv[2];
+
+if(!filename) {
+	throw Error("A filename must be specified")
+}
+
+fs.watch(filename, function() {
+	console.log("File " + filename + " just changed");
+	console.log("Watching " + filename + " for changes...");
+});
+
