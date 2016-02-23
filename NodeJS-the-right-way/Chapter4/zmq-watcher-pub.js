b98cb07017c072a0b77499c512da58df460e33f9
Wouter Van Rossem <woutervanrossem@gmail.com>
2016-02-23 13:47:30 +0100

    NodeJS - ZMQ Publish-subscribe

---
+++ NodeJS-the-right-way/Chapter4/zmq-watcher-pub.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const
+    fs = require('fs'),
+    zmq = require('zmq'),
+
+    publisher = zmq.socket('pub'),
+
+    filename = process.argv[2];
+
+fs.watch(filename, () => {
+
+    publisher.send(JSON.stringify({
+        type: 'changed',
+        file: filename,
+        timestamp: Date.now()
+    }))
+});
+
+publisher.bind('tcp://*:5432', (err) => {
+    console.log('Listening for zmq subscribers');
+});
