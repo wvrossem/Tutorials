f2f3bd335fe6e9fc8fe2d103a06ce47e69032562
Wouter Van Rossem <woutervanrossem@gmail.com>
2016-02-23 15:13:18 +0100

    NodeJS - ZMQ Requester-responder

---
+++ NodeJS-the-right-way/Chapter4/zmq-filer-rep.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const
+    fs = require('fs'),
+    zmq = require('zmq'),
+
+    responder = zmq.socket('rep');
+
+responder.on('message', (data) => {
+
+    let request = JSON.parse(data);
+    console.log('Received request to get: ' + request.path);
+
+    fs.readFile(request.path, (err, data) => {
+
+        console.log('Sending response content');
+        responder.send(JSON.stringify({
+            content: data.toString(),
+            timestamp: Date.now(),
+            pid: process.pid
+        }))
+    } );
+});
+
+responder.bind('tcp://*:5432', (err) => {
+
+    console.log('Listening for zmq requests');
+});
+
+process.on('SIGINT', () => {
+    console.log('Shutting down');
+    responder.close();
+})
